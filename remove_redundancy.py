904c2633adaa80cc6cf46f0559cb8fae6358b9ef
Glen Jasper <glen.jasper7@gmail.com>
2020-05-17 22:45:23 -0300

    Initial commit

---
+++ remove_redundancy.py
@@ -0,0 +1,439 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import sys
+import time
+import argparse
+import traceback
+import xlsxwriter
+from openpyxl import load_workbook
+from crossref.restful import Works
+
+def menu(args):
+    parser = argparse.ArgumentParser(description = "Script que elimina a redundância de pelo menos dois arquivos formatados .xlsx do Scopus, Web of Science, PubMed ou Dimensions", epilog = "Thank you!")
+    parser.add_argument("-f", "--files", required = True, help = "Arquivos formatados .xlsx, separados por vírgula")
+    parser.add_argument("-o", "--output", help = "Pasta de saida")
+    parser.add_argument("--version", action = "version", version = "%s %s" % ('%(prog)s', orr.VERSION))
+    args = parser.parse_args()
+
+    orr.INPUT_XLS_FILES = args.files
+    file_list = orr.INPUT_XLS_FILES.split(',')
+    for file in file_list:
+        file_name = os.path.basename(file)
+        file_path = os.path.dirname(file)
+        if file_path is None or file_path == "":
+            file_path = os.getcwd().strip()
+
+        this_file = os.path.join(file_path, file_name)
+        if not orr.check_path(this_file):
+            orr.show_print("%s: error: the file '%s' doesn't exist" % (os.path.basename(__file__), this_file), showdate = False)
+            orr.show_print("%s: error: the following arguments are required: -f/--files" % os.path.basename(__file__), showdate = False)
+            exit()
+
+        if os.path.basename(this_file) == orr.NAME_XLS_FILE_SCOPUS:
+            orr.XLS_FILE_SCOPUS = this_file
+        elif os.path.basename(this_file) == orr.NAME_XLS_FILE_WOS:
+            orr.XLS_FILE_WOS = this_file
+        elif os.path.basename(this_file) == orr.NAME_XLS_FILE_PUBMED:
+            orr.XLS_FILE_PUBMED = this_file
+        elif os.path.basename(this_file) == orr.NAME_XLS_FILE_DIMENSIONS:
+            orr.XLS_FILE_DIMENSIONS = this_file
+
+    if args.output is not None:
+        output_name = os.path.basename(args.output)
+        output_path = os.path.dirname(args.output)
+        if output_path is None or output_path == "":
+            output_path = os.getcwd().strip()
+
+        orr.OUTPUT_PATH = os.path.join(output_path, output_name)
+        created = orr.create_directory(orr.OUTPUT_PATH)
+        if not created:
+            orr.show_print("%s: error: Couldn't create folder '%s'" % (os.path.basename(__file__), orr.OUTPUT_PATH), showdate = False)
+            exit()
+    else:
+        orr.OUTPUT_PATH = os.getcwd().strip()
+        orr.OUTPUT_PATH = os.path.join(orr.OUTPUT_PATH, 'output_remove_redun')
+        orr.create_directory(orr.OUTPUT_PATH)
+
+class RemoveRedundancy:
+
+    def __init__(self):
+        self.VERSION = 1.0
+
+        self.INPUT_XLS_FILES = None
+        self.OUTPUT_PATH = None
+        self.DICT_XLS_FILES = {}
+
+        self.ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
+        self.LOG_NAME = "run_%s_%s.log" % (os.path.splitext(os.path.basename(__file__))[0], time.strftime('%Y%m%d'))
+        self.LOG_FILE = None
+
+        # Repositories
+        self.REPOSITORY_SCOPUS = "Scopus"
+        self.REPOSITORY_WOS = "Web of Science"
+        self.REPOSITORY_PUBMED = "PubMed"
+        self.REPOSITORY_DIMENSIONS = "Dimensions"
+
+        # Xls Summary
+        # Input
+        self.XLS_FILE_SCOPUS = None
+        self.XLS_FILE_WOS = None
+        self.XLS_FILE_PUBMED = None
+        self.XLS_FILE_DIMENSIONS = None
+        self.NAME_XLS_FILE_SCOPUS = 'input_scopus.xlsx'
+        self.NAME_XLS_FILE_WOS = 'input_wos.xlsx'
+        self.NAME_XLS_FILE_PUBMED = 'input_pubmed.xlsx'
+        self.NAME_XLS_FILE_DIMENSIONS = 'input_dimensions.xlsx'
+        # Output
+        self.XLS_FILE_OUTPUT = 'summary_unique_dois.xlsx'
+        self.XLS_SHEET_DETAIL = 'Detail'
+        self.XLS_SHEET_REDUNDANCIES = 'Redundancies'
+
+        # Xls Columns
+        self.xls_col_item = 'Item'
+        self.xls_col_title = 'Title'
+        self.xls_col_year = 'Year'
+        self.xls_col_doi = 'DOI'
+        self.xls_col_document_type = 'Document Type'
+        self.xls_col_languaje = 'Language'
+        self.xls_col_authors = 'Author(s)'
+        self.xls_col_repository = 'Repository'
+
+        self.xls_col_redundancy_type = 'Redundancy Type'
+        self.xls_val_by_doi = 'By DOI'
+        self.xls_val_by_title = 'By Title'
+
+        self.xls_columns = [self.xls_col_item,
+                            self.xls_col_title,
+                            self.xls_col_year,
+                            self.xls_col_doi,
+                            self.xls_col_document_type,
+                            self.xls_col_languaje,
+                            self.xls_col_authors,
+                            self.xls_col_repository]
+
+        # Status DOI
+        self.status_inactive_doi = 'Inactive DOIs'
+
+        # Fonts
+        self.RED = '\033[31m'
+        self.GREEN = '\033[32m'
+        self.YELLOW = '\033[33m'
+        self.BIRED = '\033[1;91m'
+        self.BIGREEN = '\033[1;92m'
+        self.END = '\033[0m'
+
+    def show_print(self, message, logs = None, showdate = True, font = None, end = None):
+        msg_print = message
+        msg_write = message
+
+        if font is not None:
+            msg_print = "%s%s%s" % (font, msg_print, self.END)
+
+        if showdate is True:
+            _time = time.strftime('%Y-%m-%d %H:%M:%S')
+            msg_print = "%s %s" % (_time, msg_print)
+            msg_write = "%s %s" % (_time, message)
+
+        print(msg_print, end = end)
+        if logs is not None:
+            for log in logs:
+                if log is not None:
+                    with open(log, 'a') as f:
+                        f.write("%s\n" % msg_write)
+                        f.close()
+
+    def start_time(self):
+        return time.time()
+
+    def finish_time(self, start, message = None):
+        finish = time.time()
+        runtime = time.strftime("%H:%M:%S", time.gmtime(finish - start))
+        if message is None:
+            return runtime
+        else:
+            return "%s: %s" % (message, runtime)
+
+    def create_directory(self, path):
+        output = True
+        try:
+            if len(path) > 0 and not os.path.exists(path):
+                os.makedirs(path)
+        except Exception as e:
+            output = False
+        return output
+
+    def check_path(self, path):
+        _check = False
+        if path is not None:
+            if len(path) > 0 and os.path.exists(path):
+                _check = True
+        return _check
+
+    def check_doi(self, doi):
+        works = Works()
+        response = works.doi(doi)
+
+        is_valid = False
+        status = None
+        if response is not None:
+            status = response['container-title'][0]
+            if status != self.status_inactive_doi:
+                is_valid = True
+
+        return is_valid
+
+    def save_xls(self, dict_unique, dict_redundancies):
+
+        def create_sheet(oworkbook, sheet_type, dictionary, styles_title, styles_rows):
+            if sheet_type == self.XLS_SHEET_REDUNDANCIES:
+                self.xls_columns.append(self.xls_col_redundancy_type)
+
+            _last_col = len(self.xls_columns) - 1
+
+            worksheet = oworkbook.add_worksheet(sheet_type)
+            worksheet.freeze_panes(row = 1, col = 0) # Freeze the first row.
+            worksheet.autofilter(first_row = 0, first_col = 0, last_row = 0, last_col = _last_col)
+            worksheet.set_default_row(height = 14.5)
+
+            # Add columns
+            for icol, column in enumerate(self.xls_columns):
+                worksheet.write(0, icol, column, styles_title)
+
+            # Add rows
+            worksheet.set_column(first_col = 0, last_col = 0, width = 7)  # Column A:A
+            worksheet.set_column(first_col = 1, last_col = 1, width = 40) # Column B:B
+            worksheet.set_column(first_col = 2, last_col = 2, width = 8)  # Column C:C
+            worksheet.set_column(first_col = 3, last_col = 3, width = 33) # Column D:D
+            worksheet.set_column(first_col = 4, last_col = 4, width = 18) # Column E:E
+            worksheet.set_column(first_col = 5, last_col = 5, width = 12) # Column F:F
+            worksheet.set_column(first_col = 6, last_col = 6, width = 36) # Column G:G
+            worksheet.set_column(first_col = 7, last_col = 7, width = 13) # Column I:I
+            if sheet_type == self.XLS_SHEET_REDUNDANCIES:
+                worksheet.set_column(first_col = 8, last_col = 8, width = 19) # Column J:J
+
+            icol = 0
+            for irow, item in dictionary.items():
+                worksheet.write(irow, icol + 0, irow, styles_rows)
+                worksheet.write(irow, icol + 1, item[self.xls_col_title], styles_rows)
+                worksheet.write(irow, icol + 2, item[self.xls_col_year], styles_rows)
+                worksheet.write(irow, icol + 3, item[self.xls_col_doi], styles_rows)
+                worksheet.write(irow, icol + 4, item[self.xls_col_document_type], styles_rows)
+                worksheet.write(irow, icol + 5, item[self.xls_col_languaje], styles_rows)
+                worksheet.write(irow, icol + 6, item[self.xls_col_authors], styles_rows)
+                worksheet.write(irow, icol + 7, item[self.xls_col_repository], styles_rows)
+                if sheet_type == self.XLS_SHEET_REDUNDANCIES:
+                    worksheet.write(irow, icol + 8, item[self.xls_col_redundancy_type], styles_rows)
+
+        workbook = xlsxwriter.Workbook(self.XLS_FILE_OUTPUT)
+
+        # Styles
+        cell_format_title = workbook.add_format({'bold': True,
+                                                 'font_color': 'white',
+                                                 'bg_color': 'black',
+                                                 'align': 'center',
+                                                 'valign': 'vcenter'})
+        cell_format_row = workbook.add_format({'text_wrap': True, 'valign': 'top'})
+
+        create_sheet(workbook, self.XLS_SHEET_DETAIL, dict_unique, cell_format_title, cell_format_row)
+        create_sheet(workbook, self.XLS_SHEET_REDUNDANCIES, dict_redundancies, cell_format_title, cell_format_row)
+
+        workbook.close()
+
+    def read_xls_summary(self, xlsfile):
+        workbook = load_workbook(filename = xlsfile, data_only = True)
+        sheet = workbook[self.XLS_SHEET_DETAIL]
+        rows = sheet.rows
+
+        file_collection = {}
+        dois = []
+        for index_i, row in enumerate(rows):
+            doi = None
+            collection = {}
+            for index_j, cell in enumerate(row):
+                if cell.value == self.xls_col_item:
+                    break
+                column_name = None
+                if index_j == 0:
+                    column_name = self.xls_col_item
+                elif index_j == 1:
+                    column_name = self.xls_col_title
+                elif index_j == 2:
+                    column_name = self.xls_col_year
+                elif index_j == 3:
+                    column_name = self.xls_col_doi
+                    doi = cell.value
+                    if doi is not None:
+                        dois.append(doi)
+                elif index_j == 4:
+                    column_name = self.xls_col_document_type
+                elif index_j == 5:
+                    column_name = self.xls_col_languaje
+                elif index_j == 6:
+                    column_name = self.xls_col_authors
+                collection.update({column_name: cell.value})
+
+            if len(collection) > 0:
+                file_collection.update({index_i: collection})
+
+        return file_collection, dois
+
+    def get_list_files(self):
+        if self.XLS_FILE_SCOPUS:
+            self.DICT_XLS_FILES.update({self.REPOSITORY_SCOPUS: self.XLS_FILE_SCOPUS})
+        if self.XLS_FILE_WOS:
+            self.DICT_XLS_FILES.update({self.REPOSITORY_WOS: self.XLS_FILE_WOS})
+        if self.XLS_FILE_PUBMED:
+            self.DICT_XLS_FILES.update({self.REPOSITORY_PUBMED: self.XLS_FILE_PUBMED})
+        if self.XLS_FILE_DIMENSIONS:
+            self.DICT_XLS_FILES.update({self.REPOSITORY_DIMENSIONS: self.XLS_FILE_DIMENSIONS})
+
+def main(args):
+    try:
+        start = orr.start_time()
+        menu(args)
+
+        orr.LOG_FILE = os.path.join(orr.OUTPUT_PATH, orr.LOG_NAME)
+        orr.XLS_FILE_OUTPUT = os.path.join(orr.OUTPUT_PATH, orr.XLS_FILE_OUTPUT)
+        orr.create_directory(orr.OUTPUT_PATH)
+        orr.get_list_files()
+        orr.show_print("#############################################################################", [orr.LOG_FILE], font = orr.BIGREEN)
+        orr.show_print("############################ Remove Redundancies ############################", [orr.LOG_FILE], font = orr.BIGREEN)
+        orr.show_print("#############################################################################", [orr.LOG_FILE], font = orr.BIGREEN)
+        orr.show_print("Input files:", [orr.LOG_FILE], font = orr.GREEN)
+        xls_file_base = {}
+        for index, (repository, file) in enumerate(orr.DICT_XLS_FILES.items()):
+            if index == 0:
+                base_repository = repository
+                base_file = file
+            orr.show_print("  %s" % file, [orr.LOG_FILE])
+        orr.show_print("", [orr.LOG_FILE])
+
+        del orr.DICT_XLS_FILES[base_repository]
+        collection_base, dois_base = orr.read_xls_summary(base_file)
+        for _, item in collection_base.items():
+            item.update({orr.xls_col_repository: base_repository})
+
+        collect_redundant = {}
+        collect_unique = {}
+        for secondary_repository, secondary_file in orr.DICT_XLS_FILES.items():
+            # Load information
+            collection_secondary, dois_secondary = orr.read_xls_summary(secondary_file)
+
+            # Get DOIs
+            dois_duplicate = list(set(dois_base) & set(dois_secondary))
+            dois_only_base = list(set(dois_base) - set(dois_secondary))
+            dois_only_secondary = list(set(dois_secondary) - set(dois_base))
+
+            # Get unique DOIs
+            collect_unique_doi = {}
+            index_u = 1
+            for _, item in collection_base.items():
+                doi = item[orr.xls_col_doi]
+                if doi in dois_duplicate:
+                    repository = '%s/%s' % (item[orr.xls_col_repository], secondary_repository)
+                    item.update({orr.xls_col_repository: repository})
+
+                collect_unique_doi.update({index_u: item})
+                index_u += 1
+
+            index_r = len(collect_redundant) + 1
+            index_u = len(collect_unique_doi) + 1
+            for _, item in collection_secondary.items():
+                doi = item[orr.xls_col_doi]
+                item.update({orr.xls_col_repository: secondary_repository})
+                if doi in dois_duplicate:
+                    item[orr.xls_col_redundancy_type] = orr.xls_val_by_doi
+                    collect_redundant.update({index_r: item})
+                    index_r += 1
+
+                if doi in dois_only_secondary:
+                    collect_unique_doi.update({index_u: item})
+                    index_u += 1
+
+            # Get redundant titles
+            nr_title = []
+            re_title = []
+            for _, row in collect_unique_doi.items():
+                flag_unique = False
+
+                title = row[orr.xls_col_title]
+                if title is not None:
+                    title = title.strip().lower()
+                    if title not in nr_title:
+                        nr_title.append(title)
+                        flag_unique = True
+
+                    if not flag_unique:
+                        re_title.append(title)
+
+            # Get unique titles
+            nr_title_ctrl = {item: {'n_check': 0, 'is_valid': False, 'repository': None} for item in re_title}
+            index_u = 1
+            index_r = len(collect_redundant) + 1
+            for _, row in collect_unique_doi.items():
+                flag_unique = False
+
+                doi = row[orr.xls_col_doi]
+                title = row[orr.xls_col_title]
+                if title is not None:
+                    title = title.strip().lower()
+
+                if title in re_title:
+                    _n_check = nr_title_ctrl[title]['n_check']
+                    _is_valid = nr_title_ctrl[title]['is_valid']
+                    _repository = nr_title_ctrl[title]['repository']
+
+                    status = False
+                    if not _is_valid:
+                        status = orr.check_doi(doi)
+
+                    if status:
+                        flag_unique = True
+                        nr_title_ctrl[title].update({'is_valid': True})
+                    else:
+                        if _n_check == 1 and _is_valid is False:
+                            flag_unique = True # forced
+
+                    if _repository is None:
+                        _repository = row[orr.xls_col_repository]
+
+                    if flag_unique:
+                        row[orr.xls_col_repository] = '%s/%s' % (_repository, secondary_repository)
+                    else:
+                        row[orr.xls_col_repository] = row[orr.xls_col_repository].split('/')[-1] # secondary_repository
+
+                    nr_title_ctrl[title].update({'n_check': _n_check + 1})
+                    nr_title_ctrl[title].update({'repository': _repository})
+                else:
+                    flag_unique = True
+
+                if flag_unique:
+                    collect_unique.update({index_u: row})
+                    index_u += 1
+                else:
+                    row[orr.xls_col_redundancy_type] = orr.xls_val_by_title
+                    collect_redundant.update({index_r: row})
+                    index_r += 1
+
+            # For other respositories
+            collection_base = collect_unique.copy()
+            dois_base = []
+            for _, item in collection_base.items():
+                dois_base.append(item[orr.xls_col_doi])
+
+        # Create summary file
+        orr.save_xls(collect_unique, collect_redundant)
+        orr.show_print("Output file: %s" % orr.XLS_FILE_OUTPUT, [orr.LOG_FILE], font = orr.GREEN)
+
+        orr.show_print("", [orr.LOG_FILE])
+        orr.show_print(orr.finish_time(start, "Elapsed time"), [orr.LOG_FILE])
+        orr.show_print("Done!", [orr.LOG_FILE])
+    except Exception as e:
+        orr.show_print("\n%s" % traceback.format_exc(), [orr.LOG_FILE], font = orr.RED)
+        orr.show_print(orr.finish_time(start, "Elapsed time"), [orr.LOG_FILE])
+        orr.show_print("Done!", [orr.LOG_FILE])
+
+if __name__ == '__main__':
+    orr = RemoveRedundancy()
+    main(sys.argv)
